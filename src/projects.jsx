8eb09db2b75aeb87782d557d6b23a844cff761ba
dakshmiyani <dakshmiyani111@gmail.com>
2025-03-08 21:17:15 +0530

    initial

---
+++ src/projects.jsx
@@ -0,0 +1,96 @@
+
+import React from 'react';
+
+
+
+ 
+
+
+function Projects(){
+return(
+    <>
+    <div className='projh'><span className='projhp'>Projects</span></div>
+       {/* 1 */}
+     <div className="projectbox">
+        <div className="dots">
+            <div className="dotsh">
+                <div className="oneball"></div>
+                <div className="twoball"></div>
+                <div className="threeball"></div>
+                <span className='projh1'>Currency Converter</span>
+            </div>
+              <div className='proj'>
+                <p><pre>
+            <span className='biopink'>const</span> project <span className='biopink'> =</span><span className='biogrey'>  &#123;</span><br />
+            <span >     name:</span><span className='biogrey'>'</span><span className='bioyellow'>Currency Converter</span><span className='biogrey'>',</span><br />
+            <span >     tools:</span><span className='biogrey'>['</span><span className='bioyellow'>HTML</span><span className='biogrey'>',</span><span className='biogrey'>'</span><span className='bioyellow'>CSS</span><span className='biogrey'>',</span><span className='biogrey'>'</span><span className='bioyellow'>Java Script</span><span className='biogrey'>']</span><br />
+             <span>     myRole:</span><span className='bioorange'>FUll Stack Developer</span><span className='biogrey'>,</span><br />
+             <span>     description:</span><span className='bioblue'>I have designed and developed a <br />
+                  <span>     Currency Converter web app using HTML, CSS, </span><br /><span>     and JavaScript. The app allows sers to </span><br />     convert currencies in real time with an <br />     intuitive  and responsive UI. It fetches live <br />     exchange rates from an API to ensure accurate <br />     conversions. The project showcases my skills <br />     in frontend development, API integration, and <br />     user experience design.</span>
+            </pre></p>
+              </div>
+            <div>
+              
+      
+    
+    
+            </div>
+       </div>
+       </div>
+
+       {/* 2 */}
+       <div className="projectbox">
+        < div className="dots">
+            <div className="dotsh">
+                <div className="oneball"></div>
+                <div className="twoball"></div>
+                <div className="threeball"></div>
+                <span className='projh1'>Password Generator</span>
+            </div>
+            <div className='proj'>
+                <p><pre>
+            <span className='biopink'>const</span> project <span className='biopink'> =</span><span className='biogrey'>  &#123;</span><br />
+            <span >     name:</span><span className='biogrey'>'</span><span className='bioyellow'>Stop Watch</span><span className='biogrey'>',</span><br />
+            <span >     tools:</span><span className='biogrey'>['</span><span className='bioyellow'>HTML</span><span className='biogrey'>',</span><span className='biogrey'>'</span><span className='bioyellow'>CSS</span><span className='biogrey'>',</span><span className='biogrey'>'</span><span className='bioyellow'>Java Script</span><span className='biogrey'>']</span><br />
+             <span>     myRole:</span><span className='bioorange'>FUll Stack Developer</span><span className='biogrey'>,</span><br />
+             <span>     description:</span><span className='bioblue'>I have designed and developed a <br />     Password Generator web app using HTML, CSS, <br />     and JavaScript. The app allows users to <br />     generate secure and random passwords with <br />     customizable length and character options, <br />     including uppercase, lowercase, numbers, <br />     and special symbols. It features a user <br />     -friendly interface and ensures strong <br />     password creation for enhanced security. This <br />     project showcases my skills in JavaScript <br />     logic, DOM manipulation, and UI/UX design.
+             </span>
+            </pre></p>
+              </div>
+         </div>    
+            
+       </div>
+       
+       {/* 3 */}
+
+       <div className="projectbox">
+        <div className="dots">
+             
+            <div className="dotsh">
+                
+                <div className="oneball"></div>
+                <div className="twoball"></div>
+                <div className="threeball"></div>
+                <span className='projh1'>Stop Watch</span>
+            </div>
+
+           
+              <div className='proj'>
+                <p><pre>
+            <span className='biopink'>const</span> project <span className='biopink'> =</span><span className='biogrey'>  &#123;</span><br />
+            <span >     name:</span><span className='biogrey'>'</span><span className='bioyellow'>Stop Watch</span><span className='biogrey'>',</span><br />
+            <span >     tools:</span><span className='biogrey'>['</span><span className='bioyellow'>HTML</span><span className='biogrey'>',</span><span className='biogrey'>'</span><span className='bioyellow'>CSS</span><span className='biogrey'>',</span><span className='biogrey'>'</span><span className='bioyellow'>Java Script</span><span className='biogrey'>']</span><br />
+             <span>     myRole:</span><span className='bioorange'>FUll Stack Developer</span><span className='biogrey'>,</span><br />
+             <span>     description:</span><span className='bioblue'>I have designed and developed a <br />     Stopwatch web app using HTML, CSS, and <br />     JavaScript. The app allows users to start,<br />     stop, and reset time with precision. It <br />     features a responsive UI and smooth <br />     functionality, making it easy to use. <br />     This project highlights my skills in <br />     DOM manipulation, event handling, and <br />     frontend development.
+             </span>
+            </pre></p>
+              </div>
+            
+       </div>
+       </div>
+    
+    </>
+);
+}
+
+export default Projects
