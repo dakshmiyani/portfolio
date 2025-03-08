ccc3e9316ea04154fa026a9860ecb27f377ba8ae
dakshmiyani <dakshmiyani111@gmail.com>
2025-03-08 21:48:33 +0530

    hi

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default {
-  // other config...
-  build: {
-    rollupOptions: {
-      external: [
-        '@fortawesome/free-solid-svg-icons',
-        '@fortawesome/react-fontawesome',
-        '@fortawesome/fontawesome-svg-core'
-      ]
-    }
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    include: [
+      '@fortawesome/fontawesome-svg-core',
+      '@fortawesome/free-solid-svg-icons',
+      '@fortawesome/free-brands-svg-icons',
+      '@fortawesome/react-fontawesome'
+    ]
   }
-}
+})
